d34d0d535a9c5357bc34d46665e7f637bc9d2ecf
lpabion <lpabion@gmail.com>
2020-12-19 17:54:23 +0100

    exercise 020

---
+++ src/exercise_020.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise_020) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define (string-delete str i)
+  (if (> (string-length str) 0)
+      (string-append (substring str 0 i) (substring str (+ i 1)))
+      str
+   )
+  )
