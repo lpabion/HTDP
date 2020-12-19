0c3aee811b5c5fca8757739bf6368e57351c5721
lpabion <lpabion@gmail.com>
2020-12-19 20:33:09 +0100

    exercise 029

---
+++ src/exercise_029.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise_029) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define BASE_PRICE 5.0)
+(define BASE_ATTENDEES 120)
+(define VARIABLE_COST 1.50)
+(define AVE_ATTENDANCE_CHANGE 15)
+(define PRICE_CHANGE_STEP 0.1)
+
+(define (attendees ticket-price)
+  (- BASE_ATTENDEES (* (- ticket-price BASE_PRICE) (/ AVE_ATTENDANCE_CHANGE PRICE_CHANGE_STEP))))
+
+(define (revenue ticket-price)
+  (* ticket-price (attendees ticket-price)))
+
+(define (cost ticket-price)
+  (* VARIABLE_COST (attendees ticket-price)))
+
+(define (profit ticket-price)
+  (- (revenue ticket-price)
+     (cost ticket-price)))
