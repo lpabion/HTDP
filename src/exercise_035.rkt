1668aa739b61fced6323a63273bdd60bab1e2c69
lpabion <lpabion@gmail.com>
2020-12-31 20:17:09 +0100

    exercise 035

---
+++ src/exercise_035.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise_035) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; String -> String
+; Extracts the last letter of string s
+; Given: "Hello", expect: "o"
+(define (get-last-letter s)
+  (string-ith s (- (string-length s) 1))
+  )
