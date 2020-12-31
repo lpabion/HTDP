102e780784f800b77d02e6299f41f1e03f70e7bc
lpabion <lpabion@gmail.com>
2020-12-31 20:37:17 +0100

    exercise 037

---
+++ src/exercise_037.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise_037) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; String -> String
+; Removes the first character of a given string (s)
+; Given: "Hello", expect: "ello"
+(define (string-rest s)
+  (substring s 1)
+  )
