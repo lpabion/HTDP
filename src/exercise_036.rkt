c6ecdf0fcda99010fcf951a324087cebe8bf01da
lpabion <lpabion@gmail.com>
2020-12-31 20:25:42 +0100

    exercise 036

---
+++ src/exercise_036.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise_036) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; Image -> Number
+; Counts the number of pixels in a given image
+; Given: picture of dimensions 10x10, expect: 100
+(define (image-area img)
+  (* (image-width img) (image-height img))
+  )
